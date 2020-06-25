e0a934f16bc34c5126ef163f2d33a8d89390848d
mmurray22 <michaelamurray2357@gmail.com>
2020-06-25 23:04:35 +0000

    WASM Setup is complete

--- portfolio/src/main/webapp/blog_script.js
+++ portfolio/src/main/webapp/blog_script.js
@@ -2,9 +2,20 @@
  * This is the JavaScript file for 
  * the blog portion of the website
  */
+import init from "./pkg/random_fxn.js";
 
 function return_home() {
    location.replace("/index.html");
-   console.log(`Return Home`);
+   console.log(`Return Home!`);
+}
+
+const randomWasm = async() => {
+   const randomFxn = await init("./pkg/random_fxn_bg.wasm");
+   
+   document.body.textContent = randomFxn.poly_pic();
+}
+function random_wasm() {
+   
+   console.log(`Random WASM Functions!`);
 }
 
