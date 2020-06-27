50983b2d464f6403a3ef28616417b914a0d5ba6b
mmurray22 <michaelamurray2357@gmail.com>
2020-06-27 00:40:37 +0000

    Makes first blog post (a WASM deployment)

---
+++ portfolio/src/main/webapp/src/lib.rs
@@ -0,0 +1,23 @@
+// The wasm-pack uses wasm-bindgen to build and generate JavaScript binding file.
+// Import the wasm-bindgen crate.
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn ops(a:i32, b:i32, op:String) -> i32 {
+    if (op == "add") {
+        return add(a, b);
+    } else if (op == "sub") {
+        return sub(a, b);
+    }
+    return 0;
+}
+
+#[wasm_bindgen]
+pub fn add(a:i32, b:i32) -> i32 {
+    a + b
+}
+
+#[wasm_bindgen]
+pub fn sub(a:i32, b:i32) -> i32 {
+    a - b
+}
