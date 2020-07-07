066db7c6651a1e332f45b35c049614b48455f806
mmurray22 <michaelamurray2357@gmail.com>
2020-07-07 21:42:10 +0000

    Almost done with basic API key integration

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -70,3 +70,11 @@ function contact() {
 		</ul>`; 
 	console.log('Contact!'); 
 }
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
