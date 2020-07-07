9f200be98ec49b212a102fae8f09be8ecf5d01f7
mmurray22 <michaelamurray2357@gmail.com>
2020-07-07 21:47:06 +0000

    Updates in response to PR comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -73,11 +73,11 @@ function contact() {
 
 function getComments() {
 	fetch('/data').then(response => response.json()).then((comments) => {
-		const commentElement = document.body;
+		const bodyElement = document.body;
         console.log('Comments: ', comments);
-        commentElement.innerHTML += 'Here are all the comments: <br>';
+        bodyElement.innerHTML += 'Here are all the comments: <br>';
         for (index in comments) {
-    		commentElement.innerHTML += comments[index] + '<br>';
+    		bodyElement.innerHTML += comments[index] + '<br>';
         }
 	});
 }
