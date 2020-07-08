b6acef4ab051a710f6c46aed2621ba745eb2cf9a
mmurray22 <michaelamurray2357@gmail.com>
2020-07-08 17:42:43 +0000

    Revert "Phase 1 of trying to customize the map color is underway"
    This reverts commit 6d0338247ae948171ccb52cc34a3189edbf0dcc2.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -75,18 +75,7 @@ function contact() {
 function createMap() {
   const map = new google.maps.Map(
       document.getElementById('map'),
-      {
-          center: {lat: 37.422, lng: -122.084}, 
-          zoom: 16,
-          styles: [
-              {
-               'featureType': 'water',
-               'elementType': 'geometry.fill',
-               'stylers': [{'colors': '#99FF33'}]
-              }
-
-          ]
-      });
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
 }
 
 function getComments() {
