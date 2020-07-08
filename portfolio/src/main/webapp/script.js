6d0338247ae948171ccb52cc34a3189edbf0dcc2
mmurray22 <michaelamurray2357@gmail.com>
2020-07-08 17:06:50 +0000

    Phase 1 of trying to customize the map color is underway

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -75,5 +75,16 @@ function contact() {
 function createMap() {
   const map = new google.maps.Map(
       document.getElementById('map'),
-      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+      {
+          center: {lat: 37.422, lng: -122.084}, 
+          zoom: 16,
+          styles: [
+              {
+               'featureType': 'water',
+               'elementType': 'geometry.fill',
+               'stylers': [{'colors': '#99FF33'}]
+              }
+
+          ]
+      });
 }
