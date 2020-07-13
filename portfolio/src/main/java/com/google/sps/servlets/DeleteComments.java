650d2d93129dff7a9c0c86c462629378f087b3f3
mmurray22 <michaelamurray2357@gmail.com>
2020-07-13 18:55:47 +0000

    Updates in response to PR comments

--- portfolio/src/main/java/com/google/sps/servlets/DeleteComments.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteComments.java
@@ -10,25 +10,25 @@
 
 package com.google.sps.servlets;
 
-import com.google.sps.servlets.DataServlet;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
-import java.io.IOException;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.google.sps.servlets.DataServlet;
+import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /** Servlet that returns some example content. */
 @WebServlet("/delete-data")
@@ -46,7 +46,6 @@ public class DeleteComments extends HttpServlet {
     if (inputNumDelete != null && !inputNumDelete.isEmpty()) {
         numDeleteComments = Integer.parseInt(inputNumDelete);
     }
-    int counter = 0;
     for (Entity entity : results.asList(FetchOptions.Builder.withLimit(numDeleteComments))) {
         datastore.delete(entity.getKey());
     }
