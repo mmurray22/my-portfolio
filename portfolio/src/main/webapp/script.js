105e89238709433f1712ae314c39858b2a90d761
mmurray22 <36782608+mmurray22@users.noreply.github.com>
2020-07-15 13:08:01 -0700

    Accidentally deleted a variable

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -56,6 +56,17 @@ function interests() {
 
 /** Creates a map and adds it to the page. */
 function loadMap() {
+    var contentString = '<div id="map-content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">'+document.getElementById("locations").value+'</h1>'+
+      '<div id="bodyContent">'+
+      '<p> This place is awesome!</p>'+
+      '<p>Unrelated Attribution: <a href="https://rustacean.net/assets/rustacean-flat-happy.svg">'+
+      'Unrelated Picture</a> '+
+      '</p>'+
+      '</div>'+
+      '</div>';
     var desiredLocationLatLng = {lat: 46.233950, lng: 6.055801};
     if (document.getElementById("locations").value == "Ramen-Nagi") {
         desiredLocationLatLng = {lat: 37.445640, lng: -122.160736};
