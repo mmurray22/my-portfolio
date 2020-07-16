552cb50a824b99db39cf4cc05bbe9645a87d1258
mmurray22 <michaelamurray2357@gmail.com>
2020-07-16 17:02:26 +0000

    Updates in response to PR comments

--- portfolio/src/main/java/com/google/sps/servlets/DeleteComments.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteComments.java
@@ -19,7 +19,6 @@ import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
-import com.google.gson.Gson;
 import com.google.sps.servlets.DataServlet;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
@@ -27,7 +26,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /** Servlet that returns some example content. */
