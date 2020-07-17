049f23a4d06c4f5094c21b34f89676ff4b99466f
mmurray22 <36782608+mmurray22@users.noreply.github.com>
2020-07-16 17:52:09 -0700

    Update portfolio/src/main/java/com/google/sps/data/MapMarker.java
    Co-authored-by: Kaveh Shahabi <shahabi@google.com>

--- portfolio/src/main/java/com/google/sps/data/MapMarker.java
+++ portfolio/src/main/java/com/google/sps/data/MapMarker.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapMarker {
+public final class MapMarker {
     private int lat;
     private int lng;
 
